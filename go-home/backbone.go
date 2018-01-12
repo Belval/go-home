9e2b9e774622ead316d32ca8914dc56542063f1f
Edouard Belval <edouard@belval.org>
2018-01-11 19:52:36 -0500

    Define base DB structure

--- go-home/backbone.go
+++ go-home/backbone.go
@@ -5,28 +5,59 @@ import (
   "net"
 )
 
-func sentUDPPacket([]byte content, string ipAddr, string port) (error) {
-  fmt.Println(string(content))
+// Used to parse and save a response from
+// a device following a Discover 'D' command
+func parseAndSaveDiscoveryPacket([]byte content) {
+
+}
+
+// Used to parse and save a response from
+// a device following a Get 'G' command.
+func parseAndSaveGetPacket([]byte content) {
+
+}
+
+func sendUDPPacket([]byte content, string ipAddr, string port) (error) {
+  conn, err = net.Dial("udp", ipAddr + ":" + port)
+  if err != nil {
+    return err
+  }
+  conn.Write(content)
+  conn.Close()
   return nil
 }
 
-func parseUDPPacket([]byte content) (error) {
-  fmt.Println(string(content))
-  return nil
+func parseUDPPacket([]byte content) {
+  switch content[0] {
+  case 'D':
+  // This is a "Discovery" packet for new devices
+    go parseAndSaveDiscoveryPacket(content)
+  case 'G':
+  // This is a "Get" response from a queried device
+    go parseAndSaveGetPacket(content)
+  default:
+    return nil
+  }
+}
+
+// Send a UDP "Discovery" packet to the
+// broadcast address.
+func sendDiscoverBroadcast() {
+  sendUDPPacket("D:", "255.255.255.255", "30000")
 }
 
 func startUDPServer(port int) {
   udpConn, err := net.ListenUDP("udp", ":" + string(port))
-	if err != nil {
-		fmt.Println(err)
-	}
+  if err != nil {
+    fmt.Println(err)
+  }
 
-	for {
+  for {
     buffer := [2048]byte{}
     offset, err := udpConn.Read(buffer[0:])
     if err != nil {
       fmt.Println("Unable to read buffer!")
     }
     parseUDPPacket(buffer[0:offset])
-	}
+  }
 }
